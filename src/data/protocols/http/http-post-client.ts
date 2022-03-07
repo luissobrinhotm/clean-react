506ccc362d2da6b0a6f884b72c72849c5a13b9b8
Luis Sobrinho <luis.sobrinho@terramagna.com.br>
2022-03-07 17:37:43 -0300

    feat: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,7 +1,9 @@
+import { HttpResponse } from '@/data/protocols/http/http-response'
+
 export type HttpPostParams = {
   url: string
   body?: object
 }
 export interface HttpPostClient {
-  post: (param: HttpPostParams) => Promise<void>
+  post: (param: HttpPostParams) => Promise<HttpResponse>
 }

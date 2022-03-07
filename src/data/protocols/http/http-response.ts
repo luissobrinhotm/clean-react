5bf5064be0ad340eaddd561d269f70d060a60cff
Luis Sobrinho <luis.sobrinho@terramagna.com.br>
2022-03-07 17:47:53 -0300

    feat: ensure RemoteAuthentication throws UnexpectedError on default cases

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -1,5 +1,9 @@
 export enum HttpStatusCode {
+  ok = 200,
   unauthorized = 401,
+  badRequest = 400,
+  notFound = 404,
+  serverError = 500,
   noContent = 204
 }
 
--- src/data/usecase/authentication/remote-authentication.ts
+++ src/data/usecase/authentication/remote-authentication.ts
@@ -2,6 +2,7 @@ import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/erros/invalid-credentials-error'
+import { UnexpectedError } from '@/domain/erros/unexpected-error'
 
 export class RemoteAuthentication {
   constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) {
@@ -10,8 +11,10 @@ export class RemoteAuthentication {
   async auth (params: AuthenticationParams): Promise<void> {
     const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      default: return await Promise.resolve()
+      case HttpStatusCode.badRequest: throw new UnexpectedError()
+      default: throw new UnexpectedError()
     }
   }
 }

4a94dba32f1d50356038d8747240a6f94093997c
Luis Sobrinho <luis.sobrinho@terramagna.com.br>
2022-03-07 17:38:21 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,12 +1,16 @@
 import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
+import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   body?: object
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent
+  }
 
-  async post (param: HttpPostParams): Promise<void> {
+  async post (param: HttpPostParams): Promise<HttpResponse> {
     this.url = param.url
     this.body = param.body
-    return await Promise.resolve()
+    return this.response
   }
 }
--- src/data/usecase/authentication/remote-authentication.spec.ts
+++ src/data/usecase/authentication/remote-authentication.spec.ts
@@ -2,6 +2,8 @@ import { RemoteAuthentication } from '@/data/usecase/authentication/remote-authe
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import faker from '@faker-js/faker'
 import { mokeAuthntication } from '@/domain/test/moke-authntication'
+import { InvalidCredentialsError } from '@/domain/erros/invalid-credentials-error'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -16,9 +18,25 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.auth(mokeAuthntication())
+    expect(httpPostClientSpy.url).toEqual(url)
+  })
+
+  test('Should call HttpPostClient with correct body', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     const authenticationParams = mokeAuthntication()
     await sut.auth(authenticationParams)
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+
+  test('Should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized
+    }
+    const promise = sut.auth(mokeAuthntication())
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
 })

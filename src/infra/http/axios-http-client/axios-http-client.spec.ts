44fef82b7d1ff32fb176cae23166a9b2358d4e3f
Luis Sobrinho <luis.sobrinho@terramagna.com.br>
2022-03-10 16:27:40 -0300

    refactor: move axios mock to a separate file

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,38 +1,33 @@
 import { AxiosHttpClient } from '@/infra/http/axios-http-client/axios-http-client'
+import { mockAxios } from '@/infra/test/mock-axios'
 import axios from 'axios'
-import faker from '@faker-js/faker'
-import { HttpPostParams } from '@/data/protocols/http'
+import { mockPostRequest } from '@/data/test/mock-http-post'
 
 jest.mock('axios')
-const mockedAxios = axios as jest.Mocked<typeof axios>
-const mockedAxiosResult = {
-  data: faker.helpers.createTransaction(),
-  status: faker.random.number()
+type SutType = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
 }
-mockedAxios.post.mockResolvedValue(mockedAxiosResult)
-const makeSut = (): AxiosHttpClient => {
-  return new AxiosHttpClient()
-}
+const makeSut = (): SutType => {
+  const sut = new AxiosHttpClient()
+  const mockedAxios = mockAxios()
 
-const mockPostRequest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.helpers.createTransaction()
-})
+  return {
+    sut, mockedAxios
+  }
+}
 
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct values', async () => {
     const request = mockPostRequest()
-    const sut = makeSut()
+    const { sut, mockedAxios } = makeSut()
     await sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 
-  test('Should return the corrent statusCode and body', async () => {
-    const sut = makeSut()
-    const httpResponse = await sut.post(mockPostRequest())
-    expect(httpResponse).toEqual({
-      statusCode: mockedAxiosResult.status,
-      body: mockedAxiosResult.data
-    })
+  test('Should return the corrent statusCode and body', () => {
+    const { sut, mockedAxios } = makeSut()
+    const promise = sut.post(mockPostRequest())
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
   })
 })

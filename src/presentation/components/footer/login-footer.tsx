c8ee63969a97580366314557a8fdbb19232ec1f0
Luis Sobrinho <luis.sobrinho@terramagna.com.br>
2022-03-15 16:12:59 -0300

    refactor: move header and footer to separate files

---
+++ src/presentation/components/footer/login-footer.tsx
@@ -0,0 +1,6 @@
+import React, { memo } from 'react'
+import Styles from './login-footer.scss'
+
+const LoginFooter: React.FC = () => <footer className={Styles.footer}/>
+
+export default memo(LoginFooter)

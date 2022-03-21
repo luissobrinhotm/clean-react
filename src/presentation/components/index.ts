0c6abc31edb861617496daab4d0c75315fbd47d4
Luis Sobrinho <luis.sobrinho@terramagna.com.br>
2022-03-21 09:58:33 -0300

    refactor: move form status to a separate file

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -2,4 +2,6 @@ export { default as LoginFooter } from '@/presentation/components/footer/login-f
 export { default as LoginHeader } from '@/presentation/components/login-header/login-header'
 export { default as Logo } from '@/presentation/components/logo/logo'
 export { default as Router } from '@/presentation/components/router/router'
+export { default as FormStatus } from '@/presentation/components/form-status/form-status'
+export { default as Input } from '@/presentation/components/input/input'
 export { default as Spinner } from '@/presentation/components/spinner/spinner'

212e029f5726906b8ce50b8adb0a2fdeed7f40ec
Luis Sobrinho <luis.sobrinho@terramagna.com.br>
2022-03-23 12:28:10 -0300

    feat: ensure Login present valid state if email is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -9,11 +9,11 @@ const Input: React.FC<Props> = (props: Props) => {
   const error = state[`${props.name}Error`]
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo Certo'
   }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
